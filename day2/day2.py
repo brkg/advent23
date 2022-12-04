f907a95bdab7b8c7156d5a370f9e6e8f7f636157
Brian Kang <brkang1@gmail.com>
2022-12-04 17:33:41 -0500

    correct logic

--- day2/day2.py
+++ day2/day2.py
@@ -1,5 +1,5 @@
 # 1 X, 2 Y, 3 Z. 0 loss, 3 draw, 6 win
-# A/X rock B/Y scissors C/Z paper
+# A/X rock B/Y paper C/Z scissors
 
 playingScore = {
   'X': 1,
@@ -15,29 +15,29 @@ def score(line):
   def resultScore():
     if a == 'A':
       if b == 'Y':
-        return 6
-      elif b == 'Z':
         return 0
+      elif b == 'Z':
+        return 6
       else:
-        return 6 
+        return 3
     elif a == 'B':
       if b == 'X':
         return 0
       elif b == 'Z':
         return 6
       else:
-        return 3 
+        return 3
     elif a == 'C':
       if b == 'X':
-        return 0 
+        return 6
       elif b == 'Y':
-        return 6 
+        return 0
       else:
         return 3 
 
   return resultScore() + playingScore[b]
 
-file = open('test.txt', 'r')
+file = open('input.txt', 'r')
 scores = [score(x) for x in file]
 file.close()
 
