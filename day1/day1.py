ebb9f28f97848486d642e431a9b1fb8ce8d3a7f0
Brian Kang <brkang1@gmail.com>
2022-12-04 16:30:23 -0500

    keep it DRY

--- day1/day1.py
+++ day1/day1.py
@@ -1,21 +1,26 @@
+# read file into array, separate out by spaces, add the rest and find max
+
 perElfArray = []
 localTotal = 0
 currentMax = 0
 
-file = open("input.txt", "r")
+def findMax():
+  global currentMax
+  global localTotal
+  if localTotal > currentMax:
+    currentMax = localTotal
 
+file = open("input.txt", "r")
 for x in file:
   if x == '\n':
     perElfArray.append(localTotal)
-    if localTotal > currentMax:
-      currentMax = localTotal
+    findMax()
     localTotal = 0
   else:
     localTotal += int(x)
 
 perElfArray.append(localTotal)
-if localTotal > currentMax:
-    currentMax = localTotal
+findMax()
 
 file.close()
 
