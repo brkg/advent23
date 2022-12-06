adff0b5b6785b179d64f3c63c68eaa5bd34095a0
Brian Kang <brkang1@gmail.com>
2022-12-05 23:53:02 -0500

    cleanup

--- 4/day4-2.py
+++ 4/day4-2.py
@@ -1,24 +1,22 @@
 def parseLine(line):
   global count
-  counted = False
   first = line.split(',')[0].split('-')
   second = line.split(',')[1].split('-')
   a = int(first[0])
   b = int(first[1])+1
   c = int(second[0])
   d = int(second[1])+1
+  
   if a >= c:
     for i in range(c, d):
       if i == a or i == b:
-        counted = True
+        count += 1
         break
   else:
     for i in range(a, b):
       if i == c or i == d:
-        counted = True
+        count += 1
         break
-  if counted:
-    count += 1
 
 count = 0
 file = open('input.txt', 'r')
